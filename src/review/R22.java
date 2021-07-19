f99a1acb568b2f32737f0d2549eb2fa8ed3f3ab9
jane <82953024+git-jane-hub@users.noreply.github.com>
2021-07-19 22:50:24 +0900

    review

---
+++ src/review/R22.java
@@ -0,0 +1,25 @@
+package review;
+class Data_1 {
+	int value;
+//	Data_1(){}		// 기본 생성자가 없으면 컴파일러가 자동으로 추가 
+		
+	}
+
+
+class Data_2 {
+	int value;
+	Data_2(){		// 기본 생성자 추
+		
+	}
+	Data_2(int x){
+		
+	}
+}
+
+public class R22 {
+	public static void main(String[] args) {
+		Data_1 d1 = new Data_1();
+		Data_2 d2 = new Data_2();
+		
+	}
+}
